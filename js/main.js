170c004775abca9b34b186d160a620752d2fad71
Fazeel Ahmed Khan <fazeelahmedkhan15@gmail.com>
2021-09-19 10:29:15 -0400

    left row added

--- js/main.js
+++ js/main.js
@@ -8,5 +8,61 @@ $(document).ready(function(){
     $("#modal_btn").click(function(){
         $("#main_modal").modal();
     });
+
+    $("#modal_btn_two").click(function(){
+        $("#main_modal").modal();
+    });
+
+    $("#modal_btn_three").click(function(){
+        $("#main_modal").modal();
+    });
+
+    $("#modal_btn_four").click(function(){
+        $("#main_modal").modal();
+    });
+
+    $("#modal_btn_five").click(function(){
+        $("#main_modal").modal();
+    });
+
+    $("#modal_btn_six").click(function(){
+        $("#main_modal").modal();
+    });
+
+    $("#modal_btn_seven").click(function(){
+        $("#main_modal").modal();
+    });
+
+    $("#modal_btn_eight").click(function(){
+        $("#main_modal").modal();
+    });
+
+    $("#modal_btn_nine").click(function(){
+        $("#main_modal").modal();
+    });
+
+    $("#modal_btn_ten").click(function(){
+        $("#main_modal").modal();
+    });
+
+    $("#modal_btn_eleven").click(function(){
+        $("#main_modal").modal();
+    });
+
+    $("#modal_btn_twelve").click(function(){
+        $("#main_modal").modal();
+    });
+
+    $("#modal_btn_thirteen").click(function(){
+        $("#main_modal").modal();
+    });
+
+    $("#modal_btn_fourteen").click(function(){
+        $("#main_modal").modal();
+    });
+
+    $("#modal_btn_fifteen").click(function(){
+        $("#main_modal").modal();
+    });
 });
   
